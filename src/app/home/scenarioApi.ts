0d42658855b97b8f47cb5fe2c99245a7278370d5
poul0102 <lday217617@gmail.com>
2025-11-24 15:20:55 +0900

    [refactor] 공통 apiGet 유틸 응답 타입 개선 및 검증 로직 추가

--- src/app/home/scenarioApi.ts
+++ src/app/home/scenarioApi.ts
@@ -2,9 +2,12 @@ import { CompletedScenario, ScenarioProgress } from "@/types";
 import axiosInstance from "@/utils/axiosInstance";
 
 const apiGet = async <T>(url: string): Promise<T> => {
-  const res = await axiosInstance.get<{ data: T }>(url);
-  return res.data.data;
-}
+  const { data } = await axiosInstance.get(url);
+  if (!data?.data) {
+    throw new Error(`Invalid API response format for ${url}`);
+  }
+  return data.data as T;
+};
 
 // API 호출 함수 : 완료 시나리오 조회
 export const fetchCompletedScenarios = () => apiGet<CompletedScenario[]>("/users/me/scenarios/completed");
