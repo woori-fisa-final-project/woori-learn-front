d0ef2b2b8e05bb659700fbb8f984bb81f8a9b563
junn34 <lucasorjh3412@gmail.com>
2025-11-28 16:38:12 +0900

    [refactor] axiosInstance.ts baseurl 8080 url 추가

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -14,7 +14,8 @@ let refreshPromise: Promise<string> | null = null;
 
 const axiosInstance = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  baseURL: "", // 테스트용으로 proxy 설정
+  // baseURL: ""
+  baseURL: "http://localhost:8080", // 테스트용으로 proxy 설정
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -23,7 +24,7 @@ const axiosInstance = axios.create({
 
 // 토큰 갱신 전용 axios 인스턴스
 const refreshAxios = axios.create({
-  baseURL: "",
+  baseURL: "http://localhost:8080",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
